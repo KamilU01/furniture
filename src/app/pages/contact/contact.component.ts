37027f2249ace61c6213f5da0da97409b8a99e0c
Kamil Ujec <kamil.ujec@gmail.com>
2021-07-12 12:52:39 +0200

    Dodanie strony "Kontakt"

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  isSuccess: boolean = false;
+  isError: boolean = false;
+  isLoading: boolean = false;
+  isNotValid: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      this.isNotValid = true;
+      return
+    }
+
+    const name = form.value.name;
+    const phone = form.value.phone;
+    const email = form.value.email;
+    const message = form.value.text;
+
+    this.isLoading = true;
+
+    /* this.contactService.sendMessage(name, phone, email, message).subscribe(res => {
+      this.isNotValid = false;
+      this.isLoading = false;
+      this.isSuccess = true;
+      this.isError = false;
+    }, err => {
+      this.isNotValid = false;
+      this.isLoading = false;
+      this.isSuccess = false;
+      this.isError = true;
+    }) */
+  }
 }
