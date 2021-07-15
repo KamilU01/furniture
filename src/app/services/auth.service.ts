aaed2c96c634082bff57fc80e195064a6ea3403b
Kamil Ujec <kamil.ujec@gmail.com>
2021-07-15 10:31:15 +0200

    Poprwaiony routing

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -100,7 +100,7 @@ export class AuthService {
 
   logoutUser() {
     this.loggedUser = null;
-    this.router.navigate(["/autoryzacja"])
+    this.router.navigate(["/"])
     this.removeTokens();
     this.USER.next(null)
     clearTimeout(this.tokenExpirationTimer)
