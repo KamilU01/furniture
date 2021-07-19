d7f3e2a717545eaa88fd4cfb7537d326446a7197
Kamil Ujec <kamil.ujec@gmail.com>
2021-07-19 11:57:19 +0200

    Aranżacje na stronie głównej

--- src/app/pages/homepage/sections/arrangements/arrangements.component.ts
+++ src/app/pages/homepage/sections/arrangements/arrangements.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Arrangment } from 'src/app/models/graphql';
+import { ShopService } from 'src/app/services/shop.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-arrangements',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./arrangements.component.scss']
 })
 export class ArrangementsComponent implements OnInit {
+  isLoading: boolean = true;
+  arrangments!: Array<Arrangment>;
+  url = environment.apiUrl;
 
-  constructor() { }
+  constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
+    this.shopService.getArrangments().subscribe(res => {
+      this.arrangments = res.data.arrangments.slice(0, 8);
+      this.isLoading = false;
+    })
   }
-
 }
